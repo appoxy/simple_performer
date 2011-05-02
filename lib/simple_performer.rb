2b832fd2579447b877c2f57d553d37a9d02f6593
Travis Reeder <travis@appoxy.com>
2011-05-02 15:47:46 -0700

    Changed method name to time and puts_duration now returns the duration

--- lib/simple_performer.rb
+++ lib/simple_performer.rb
@@ -12,160 +12,182 @@ require_relative 'sp_rack'
 #for Now data array would simply be a queue
 module SimplePerformer
 
-    @@metrics_path = '/api/metrics'
-    @@base_url = 'http://incoming.simpleperformr.com' + @@metrics_path
+  @@metrics_path = '/api/metrics'
+  @@base_url = 'http://incoming.simpleperformr.com' + @@metrics_path
 
-    class << self
-        attr_accessor :config,
-                      :service
+  class << self
+    attr_accessor :config,
+                  :service
 
-        def configure()
-            SimplePerformer.config ||= SimplePerformer::Config.new
-            yield(config)
+    def configure()
+      SimplePerformer.config ||= SimplePerformer::Config.new
+      yield(config)
 #            SimplePerformer.service = Performr.new(config.access_key, config.secret_key, :config=>config)
-            # todo: should delay start until used?
-            Performr.start
-        end
+      # todo: should delay start until used?
+      Performr.start
     end
+  end
 
-    # name is what this chunk of code will be referred to in the UI.
-    def self.benchmark(name, &block)
-        Performr.benchmark(name, &block)
-    end
+  # name is what this chunk of code will be referred to in the UI.
+  def self.benchmark(name, &block)
+    Performr.benchmark(name, &block)
+  end
 
-    def self.shutdown
+  def self.shutdown
 #        EventMachine.stop
+  end
+
+  def self.base_url=(url)
+    @@base_url = url + @@metrics_path
+  end
+
+  def self.base_url
+    @@base_url
+  end
+
+  # Simple function that simply spits out the duration of the block
+  # - name is for reference.
+  def self.puts_duration(name, &block)
+    start_time = Time.now
+    yield
+    end_time = Time.now
+    duration = end_time-start_time
+    puts "#{name} duration: #{duration} seconds."
+    duration
+  end
+
+  class Aggregator
+    def initialize
+      @aggs = {}
     end
 
-    def self.base_url=(url)
-        @@base_url = url + @@metrics_path
+
+    def time(name, &block)
+      start_time = Time.now
+      yield
+      end_time = Time.now
+      agg = @aggs[name]
+      agg = Agger.new(name) if agg.nil?
+      agg.add(end_time-start_time)
+      @aggs[name] = agg
+
     end
 
-    def self.base_url
-        @@base_url
+    def to_s
+      s = ''
+      @aggs.each_pair do |k, v|
+        s << v.to_s + "\n"
+      end
+      s
     end
 
-    # Simple function that simply spits out the duration of the block
-    # - name is for reference.
-    def self.puts_duration(name, &block)
-        start_time = Time.now
-        yield
-        end_time = Time.now
-        puts "#{name} duration: #{(end_time-start_time)} seconds."
+    # Prints current info
+#    def puts(name)
+#      agg = @aggs[name]
+#      agg.puts
+#    end
+  end
+
+  class Agger
+    attr_accessor :name, :sum, :count
+
+    def initialize(name)
+      @name = name
+      @sum = 0.0
+      @count = 0
     end
 
-    class Aggregator
-        def initialize
-            @aggs = {}
-        end
-
-        def benchmark(name, &block)
-            start_time = Time.now
-            yield
-            end_time = Time.now
-            agg = @aggs[name]
-            agg = Agger.new(name) if agg.nil?
-            agg.add(end_time-start_time)
-            @aggs[name] = agg
-
-        end
-
-        # Prints current info
-        def puts(name)
-            agg = @aggs[name]
-            agg.puts
-        end
+    def add(duration)
+      @sum += duration
+      @count += 1
     end
 
-    class Agger
-        attr_accessor :name, :sum, :count
-
-        def initialize(name)
-            @name = name
-            @sum = 0.0
-            @count = 0
-        end
-
-        def add(duration)
-            @sum += duration
-            @count += 1
-        end
-
-        def puts
-            Kernel.puts "Aggregator #{name}: count=#{count} avg=#{avg}"
-        end
+    def avg
+      if @count && @count > 0
+        return 1.0 * @sum / @count
+      end
+      return 0.0
     end
 
-    class Performr
-        #< ApiAuth
+    def to_s
+      "Aggregator #{name}: count=#{count} avg=#{avg}"
+    end
 
-        class <<self
+#    def puts
+#      Kernel.puts "Aggregator #{name}: count=#{count} avg=#{avg}"
+#    end
+  end
 
-            attr_accessor :data, :api_key, :base_uri, :timer
+  class Performr
+    #< ApiAuth
 
-            def config options={}, &blk
-                SimplePerformer.configure do |config|
-                    config.access_key = options[:access_key]
-                    config.host = options[:host]
-                end
+    class <<self
 
-                instance_eval &blk if block_given?
-            end
+      attr_accessor :data, :api_key, :base_uri, :timer
 
-            def start
-                self.data = Queue.new
+      def config options={}, &blk
+        SimplePerformer.configure do |config|
+          config.access_key = options[:access_key]
+          config.host = options[:host]
+        end
 
-                puts "api_key=" + api_key.to_s
-                if api_key
-                    # only start it if we passed in a key
-                    run_update
-                end
+        instance_eval &blk if block_given?
+      end
 
-            end
+      def start
+        self.data = Queue.new
 
-            def reset_queue
-                self.data = Queue.new
-            end
+        puts "api_key=" + api_key.to_s
+        if api_key
+          # only start it if we passed in a key
+          run_update
+        end
 
-            def send_update
+      end
+
+      def reset_queue
+        self.data = Queue.new
+      end
+
+      def send_update
 #                puts "send_update api_key=" + api_key
-                url = "/update_metrics/"+api_key
-                #consumer for data
-                #delete all data from queue after update
-                to_send = return_data
+        url = "/update_metrics/"+api_key
+        #consumer for data
+        #delete all data from queue after update
+        to_send = return_data
 #                puts "sending json=" + to_send.inspect
 
-                to_send = to_send.to_json
+        to_send = to_send.to_json
 #                puts 'posting to ' + full_url(url)
-                response = RestClient.post(full_url(url), to_send, :content_type => :json)
-            end
+        response = RestClient.post(full_url(url), to_send, :content_type => :json)
+      end
 
-            def return_data
-                avg_metrics = {}
-                i=0
-                data = self.data
-                reset_queue
-                # create a new one so the current queue doesn't have the opportunity to keep filling up and we try to keep popping too
-                until data.empty?
-                    metric=data.pop
-                    name=metric[:name]
-                    avg_metrics[name] ||= {:count => 0, :user => 0, :system => 0, :total => 0, :real => 0}
-                    avg_metrics[name][:count]   += 1
-                    avg_metrics[name][:user]    += metric[:user]
-                    avg_metrics[name][:system]  += metric[:system]
-                    avg_metrics[name][:total]   += metric[:total]
-                    avg_metrics[name][:real]    += metric[:real]
-                end
-                #            puts hash.inspect + " hash inspect"
-                avg_metrics
-            end
+      def return_data
+        avg_metrics = {}
+        i=0
+        data = self.data
+        reset_queue
+        # create a new one so the current queue doesn't have the opportunity to keep filling up and we try to keep popping too
+        until data.empty?
+          metric=data.pop
+          name=metric[:name]
+          avg_metrics[name] ||= {:count => 0, :user => 0, :system => 0, :total => 0, :real => 0}
+          avg_metrics[name][:count] += 1
+          avg_metrics[name][:user] += metric[:user]
+          avg_metrics[name][:system] += metric[:system]
+          avg_metrics[name][:total] += metric[:total]
+          avg_metrics[name][:real] += metric[:real]
+        end
+        #            puts hash.inspect + " hash inspect"
+        avg_metrics
+      end
 
 
-            def full_url(path)
-                SimplePerformer.base_url + path
-            end
+      def full_url(path)
+        SimplePerformer.base_url + path
+      end
 
-            def periodic_update
+      def periodic_update
 
 #                EventMachine.run do
 #                    @timer = EventMachine::PeriodicTimer.new(60) do
@@ -180,50 +202,50 @@ module SimplePerformer
 #
 #                    end
 #                end
-            end
+      end
 
-            def cancel_update
-                timer.cancel if timer
-            end
+      def cancel_update
+        timer.cancel if timer
+      end
 
 
-            def run_update
-                Thread.new do
-                    periodic_update
-                end
-            end
-
-            def benchmark name, &block
-                opts = name
-                stat=Benchmark::measure &block
-                puts 'name2=' + name.inspect
-                if opts.is_a? Hash
-                    name = opts[:name]
-                end
-                unless name && name.length > 0
-                    raise "Must provide a name for benchmark."
-                end
-                puts 'name =' + name
-                pp stat.to_hash, stat.class
-                collect_stats stat.to_hash.merge(:name => name)
-            end
-
-            def collect_stats stat
-                self.data.push(stat)
-            end
-
+      def run_update
+        Thread.new do
+          periodic_update
         end
+      end
+
+      def benchmark name, &block
+        opts = name
+        stat=Benchmark::measure &block
+        puts 'name2=' + name.inspect
+        if opts.is_a? Hash
+          name = opts[:name]
+        end
+        unless name && name.length > 0
+          raise "Must provide a name for benchmark."
+        end
+        puts 'name =' + name
+        pp stat.to_hash, stat.class
+        collect_stats stat.to_hash.merge(:name => name)
+      end
+
+      def collect_stats stat
+        self.data.push(stat)
+      end
+
     end
+  end
 end
 
 class Benchmark::Tms
-    def to_hash
-        {
-                :user => @utime,
-                :real => @real,
-                :total => @total,
-                :system =>@stime
-        }
-    end
+  def to_hash
+    {
+        :user => @utime,
+        :real => @real,
+        :total => @total,
+        :system =>@stime
+    }
+  end
 end
 
